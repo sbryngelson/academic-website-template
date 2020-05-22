1dfd9d6c2c3b4deed7eca9b253e98388d993ea75
Spencer Bryngelson <sbryngelson@gmail.com>
2020-05-21 23:23:02 -0700

    minor changes to bibtex to html utility. actually works now.

--- readyaml.py
+++ readyaml.py
@@ -1,4 +1,4 @@
-#! /Library/Frameworks/Python.framework/Versions/3.8/bin/python3
+#!/usr/bin/env python3
 
 import yaml
 import os
@@ -16,37 +16,31 @@ if is_tool('arxiv2bib'):
                 if (k=='url'):
                     url=v
                 if (k=='arxiv'):
+                    print('working on ArXiv file ' + url)
                     os.system("arxiv2bib " + str(v) + " > papers/" + url +".bib")
-            if is_tool('bibtex2html'):
-                os.system("bibtex2html -o papers/" + url + " papers/" + url + ".bib")
-                os.system("mv papers/" + url + "_bib.html  papers/" + url + ".html")
-            else: 
-                os.system("mv papers/" + url + ".bib" "papers/" + url + ".html")
+                    if is_tool('bibtex2html'):
+                        os.system("export TMPDIR=.; bibtex2html -q -o papers/" + url + " papers/" + url + ".bib")
+                        os.system("mv papers/" + url + "_bib.html  papers/" + url + ".html")
+                    else: 
+                        os.system("mv papers/" + url + ".bib" "papers/" + url + ".html")
 else:
     print('Command line tool arxiv2bib not found. Arxiv files not converted to bibliographic files .bib. \
             I believe this requires python3 (like this script). \
-            Consider installing this tool via pip install arxiv2bib or pip3 install arxiv2bib')
+            Consider installing this tool via pip3 install arxiv2bib')
 
 if is_tool('doi2bib'):
     if not is_tool('bibtex2html'):
         print('Command line tool bibtex2html not found. Will progress, but nicer bib files will be generated if this tool is installed')
+        print('This tool can be downloaded from: https://www.lri.fr/~filliatr/bibtex2html')
+
     with open(r'_data/publist.yml') as file:
-        # The FullLoader parameter handles the conversion from YAML
-        # scalar values to Python the dictionary format
         pubs = yaml.load(file, Loader=yaml.FullLoader)
-        # i = 0
         for pub in pubs:
-            # i=i+1
-            # print('publication #',i)
             for k,v in pub.items():
-                # print(k,v)
                 if (k=='url'):
-                    # print(k,'->',v)
-                    # trim 3 digit extension
-                    # url=v[:-4]
                     url=v
                 if (k=='doi'):
-                    # print(k,'->',v)
+                    print('working on DOI file ' + url)
                     os.system("doi2bib " + str(v) + " > papers/" + url +".bib")
                     #remove empty lines that doi2bib gives
                     open("papers/tmp.bib",'w').write(
@@ -54,7 +48,7 @@ if is_tool('doi2bib'):
                             l for l in open("papers/" + url + ".bib") if l.strip()))
                     os.system("mv papers/tmp.bib papers/" + url + ".bib")
                     if is_tool('bibtex2html'):
-                        os.system("bibtex2html -o papers/" + url + " papers/" + url + ".bib")
+                        os.system("export TMPDIR=.; bibtex2html -q -o papers/" + url + " papers/" + url + ".bib")
                         os.system("mv papers/" + url + "_bib.html  papers/" + url + ".html")
                     else: 
                         os.system("mv papers/" + url + ".bib" "papers/" + url + ".html")
