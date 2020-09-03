49f407f459d41bc9962f1c9a2200e84713e0fdda
Spencer Bryngelson <sbryngelson@gmail.com>
2020-09-03 10:38:56 -0700

    shorter bib files and a script that writes them

--- makebib.py
+++ makebib.py
@@ -14,6 +14,13 @@ with open(r'_data/publist.yml') as file:
             if (k=='arxiv'):
                 print('working on ArXiv file ' + url)
                 os.system("./scripts/arxiv2bib.py " + str(v) + " > papers/" + url +".txt")
+                infile = open("papers/" + url +".txt",'r')
+                newopen = open("papers/tmp.bib",'w')
+                for line in infile:
+                    if 'File' not in line and 'Eprint' not in line and 'PrimaryClass' not in line:
+                        newopen.write(line)
+                newopen.close()
+                os.system("mv papers/tmp.bib papers/" + url + ".txt")
 
 #get .bib for files with DOI 
 with open(r'_data/publist.yml') as file:
@@ -29,4 +36,12 @@ with open(r'_data/publist.yml') as file:
                 open("papers/tmp.bib",'w').write(
                     ''.join(
                         l for l in open("papers/" + url + ".txt") if l.strip()))
-                os.system("mv papers/tmp.bib papers/" + url + ".txt")
+                # os.system("mv papers/tmp.bib papers/" + url + ".txt")
+                infile = open("papers/tmp.bib",'r')
+                newopen = open("papers/tmp2.bib",'w')
+                for line in infile:
+                    if 'url' not in line and 'publisher' not in line:
+                        newopen.write(line)
+                newopen.close()
+                os.system("mv papers/tmp2.bib papers/" + url + ".txt")
+                os.system("rm -f papers/tmp*.bib")
