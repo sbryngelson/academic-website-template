f6a90794681100a07664eb24ffe35570add2e95f
sliao7 <sliao7@gatech.edu>
2024-05-30 19:48:14 -0400

    update bootstrap

--- update_bootstrap.sh
+++ update_bootstrap.sh
@@ -2,8 +2,10 @@
 
 # A useful script to download the latest version of bootstrap and jquery
 
-rm -rf node_modules package-lock.json
-npm install bootstrap@4 jquery@3 @popperjs/core
+# rm -rf node_modules package-lock.json
+mkdir node_modules
+# bBootstrap 5 is designed to be used without jQuery
+npm install bootstrap@5
 
 rm -rf _sass/bootstrap
 mkdir -p _sass/bootstrap
@@ -13,7 +15,6 @@ touch _sass/bootstrap/__DO_NOT_MODIFY
 rm -rf assets/javascript/bootstrap
 mkdir -p assets/javascript/bootstrap
 cp node_modules/bootstrap/dist/js/bootstrap.bundle.min.* assets/javascript/bootstrap/
-cp node_modules/jquery/dist/jquery.min.* assets/javascript/bootstrap/
 touch assets/javascript/bootstrap/__DO_NOT_MODIFY
 
 # popper stuff
